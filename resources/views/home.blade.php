fc5cb94262ab59940ff44d46c6054504bd44fa5b
AimLesson <manuelsugianto@gmail.com>
2025-01-10 18:50:25 +0700

    grid update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,30 +21,30 @@
     {{-- SEKOLAH --}}
     <section class="text-gray-600 body-font">
         <div class="container mt-10 px-5 py-6 mx-auto bg-white rounded-lg">
-          <div class="flex flex-wrap w-full mb-10">
-            <div class="lg:w-1/2 w-full mb-2 lg:mb-0">
-              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Sekolah Kami</h1>
-              <div class="h-1 w-20 bg-blue-500 rounded"></div>
+            <div class="flex flex-wrap w-full mb-10">
+                <div class="lg:w-1/2 w-full mb-2 lg:mb-0">
+                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Sekolah Kami</h1>
+                    <div class="h-1 w-20 bg-blue-500 rounded"></div>
+                </div>
             </div>
-          </div>
-          <div class="flex flex-wrap -m-4">
-            @foreach($branch as $b)
-            <a href="{{ route('branches.show', $b->id) }}" class="xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 w-full">
-              <div class="bg-gray-100 p-6 rounded-lg h-64 w-80 mx-auto border-b shadow">
-                <!-- Avatar Image -->
-                <img 
-                  class="h-40 w-40 rounded-full object-cover object-center mx-auto mb-6 border-4 border-gray-300" 
-                  src="{{ asset('storage/' . $b->logo) }}" 
-                  alt="content"
-                >
-                <h2 class="text-lg text-gray-900 font-medium title-font mb-4 text-center">{{ $b->name }}</h2>
-              </div>
-            </a>
-            @endforeach
-          </div>
-          
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+                @foreach($branch as $b)
+                <a href="{{ route('branches.show', $b->id) }}" class="block">
+                    <div class="bg-gray-100 p-6 rounded-lg h-64 w-full mx-auto border-b shadow">
+                        <!-- Avatar Image -->
+                        <img 
+                            class="h-40 w-40 rounded-full object-cover object-center mx-auto mb-6 border-4 border-gray-300" 
+                            src="{{ asset('storage/' . $b->logo) }}" 
+                            alt="content"
+                        >
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4 text-center">{{ $b->name }}</h2>
+                    </div>
+                </a>
+                @endforeach
+            </div>
         </div>
     </section>
+
 
     {{-- Berita --}}
     <section class="text-gray-600 body-font">
